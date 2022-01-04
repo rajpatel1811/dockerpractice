45a6b767ec58f7aae91e448c92101123676631fa
rajpatel1811 <49684876+rajpatel1811@users.noreply.github.com>
2022-01-03 19:16:54 -0800

    Add files via upload

---
+++ project/src/server.py
@@ -0,0 +1,18 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import datetime
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/greetings':
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write(bytes(datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S"), "utf8"))
+        else:
+            self.send_response(400)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            message = "Invalid endpoint"
+            self.wfile.write(bytes(message, "utf8"))
+
+with HTTPServer(('', 8181), handler) as server:
+    server.serve_forever()
